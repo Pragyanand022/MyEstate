ca83746502612b2e5c1c5fb9edbd3c89a4c5182a
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2023-11-18 13:49:52 +0530

    adding signin function

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.listen(3000,()=>{
     console.log("server is running on port 3000!!");
 });
-
+ 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 
