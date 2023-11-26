9c5223c81999af7a0477777d8fd4852d5d5e614d
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2023-11-26 23:47:01 +0530

    complete profile page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 export default function Header() {
-  const {currentUser} = useSelector(state => state.user);
+  const {currentUser} = useSelector((state) => state.user);
   return (
     <header className='bg-slate-200 shadow-md '>
       <div className='flex justify-between max-w-6xl m-auto items-center p-3'>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
-export default function Profile() {
+export default function 
+() {
+  const {currentUser} = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+       <h1 className='font-semibold text-3xl mt-7 text-center'>Profile</h1>
+       <form className='flex flex-col gap-4'>
+          <img className='rounded-full h-24 w-24 object-cover self-center my-3 cursor-pointer' src={currentUser.avatar} alt='profile-img'/>
+          <input type="text" placeholder='username' className='border rounded-lg p-3 ' id='username'/>
+          <input type="email" placeholder='email' className='border rounded-lg p-3 ' id='email'/>
+          <input type="password" placeholder='password' className='border rounded-lg p-3 ' id='password'/>
+          <button className='uppercase bg-slate-700 p-3 rounded-lg text-white hover:opacity-95 disabled:opacity-80'>Update</button>
+        </form>
+        <div className='flex justify-between mt-5'>
+          <span className='text-red-700 cursor-pointer'>Delete account</span>
+          <span className='text-red-700 cursor-pointer'>Sign out</span>
+        </div>
+    </div>
   )
 }
