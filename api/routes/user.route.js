3da33a554b10c3bff335981f30cb765afe9844b5
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2023-12-01 17:25:02 +0530

    create getUserListings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, test, upadateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, upadateUser, getUserListings} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUsers.js";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/test',test);
 router.post('/update/:id',verifyToken,upadateUser);
 router.delete('/delete/:id',verifyToken,deleteUser);
+router.get('/listings/:id',verifyToken,getUserListings);
 
 
 export default router;
