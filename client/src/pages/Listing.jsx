ba85135b050485be2e1c31e197d372d8ed8673eb
Pragyanand022 <99154355+Pragyanand022@users.noreply.github.com>
2023-12-07 15:52:19 +0530

    complete listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import { FaBath, FaBed, FaChair, FaMapMarkerAlt, FaParking } from "react-icons/fa";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
@@ -32,8 +33,7 @@ export default function Listing() {
     };
     fetchListing();
   }, [params.listingId]);
-  console.log(listing)
-
+  console.log(listing);
 
   return (
     <main>
@@ -46,18 +46,76 @@ export default function Listing() {
         </p>
       )}
       {listing && !error && !loading && (
-        <div>
-          <Swiper navigation>
-            {listing.imageUrls.map((url) => (
-              <SwiperSlide key={url}>
-                <div
-                  className="h-[550px]"
-                  style={{ background: `url(${url}) center no-repeat`, backgroundSize:'cover' }}
-                ></div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
+        <>
+          <div>
+            <Swiper navigation>
+              {listing.imageUrls.map((url) => (
+                <SwiperSlide key={url}>
+                  <div
+                    className="h-[550px]"
+                    style={{
+                      background: `url(${url}) center no-repeat`,
+                      backgroundSize: "cover",
+                    }}
+                  ></div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+          <div className="p-4 w-full sm:max-w-[1000px] m-auto">
+            <h1 className="font-bold text-2xl">
+              {listing.name} - ${listing.regularPrice}
+            </h1>
+            <div className="flex items-center gap-3 text-sm font-semibold mt-4">
+              <FaMapMarkerAlt className="text-green-700 " />
+              <span className="mb-1 text-slate-700">{listing.address}</span>
+            </div>
+            <div className="flex mt-3 gap-3 font-semibold">
+              <p className="bg-red-900 w-full max-w-[200px] text-white text-center rounded-lg p-1">
+                {listing.type === "rent" ? "For Rent" : "For Sale"}
+              </p>
+              {listing.offer && (
+                <p className="bg-green-900 w-full max-w-[200px] text-white text-center rounded-lg p-1">
+                  $ {listing.regularPrice - listing.discountPrice}
+                </p>
+              )}
+            </div>
+            <div className="flex mt-3">
+              <p className="text-slate-800 font-semibold text-sm">
+                <span className="object-cover font-bold text-black text-md">Details - </span>
+                {listing.description}
+              </p>
+            </div>
+            <div className="flex mt-3  text-green-800 font-semibold gap-5">
+              <div className="flex items-center gap-1">
+                <FaBed />
+                <span>
+                  {listing.bedrooms}
+                  {(listing.bedrooms>1)?" Beds":" Bed"}
+                </span>
+              </div>
+              <div className="flex items-center gap-1">
+                <FaBath />
+                <span>
+                  {listing.bathrooms}
+                  {(listing.bathrooms>1)?" Baths":" Bath"}
+                </span>
+              </div>
+              <div className="flex items-center gap-1">
+                <FaParking />
+                <span>
+                  {(listing.parking)?" Parking":" No Parking"}
+                </span>
+              </div>
+              <div className="flex items-center gap-1">
+                <FaChair />
+                <span>
+                  {(listing.furnished)?" Furnished":" Not Frunished"}
+                </span>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </main>
   );
